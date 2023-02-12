9e90563929557cd4fb80ca08d9d50969314d68f3
M-Ridha <harhira.mouhamedridha@gmail.com>
2023-02-12 11:44:23 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Profile from "./Profile";
 import img from './logo.svg'
+import Gittest from "./Gittest";
 function App() {
  
   const show=(a)=>{
@@ -8,7 +9,7 @@ function App() {
   return (
    <div>
     <Profile fullname="mootez" bio="software engineer" profession="student" fct={show}>{img}</Profile>
-  
+    <Gittest/>
    </div>
   );
 }
---
+++ src/Gittest.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gittest = () => {
+  return (
+    <div>
+        yali nsiti 5alek 
+    </div>
+  )
+}
+
+export default Gittest
